fa9779f79cb3947234c8745dc6bb21b0f29e52e4
Usuario invitado <Guest@iMac-de-PUCE-A107-09.local>
2026-05-26 16:28:51 -0500

    Segundo comit 26 de mayo

--- app/src/main/java/ec/edu/puce/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/puce/githubclient/MainActivity.kt
@@ -4,24 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-
-import ec.edu.puce.githubclient.ui.screens.RepoLs
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import ec.edu.puce.githubclient.ui.theme.GithubClientTheme
+import ec.edu.puce.githubclient.ui.screens.RepoList
+import ec.edu.puce.githubclient.ui.screens.RepoFrom
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GithubClientTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    RepoLs(
 
-                        modifier = Modifier.padding(innerPadding)
+            var currentScreen by remember { mutableStateOf("repolist") }
+
+            GithubClientTheme {
+                when(currentScreen) {
+                    "repolist" -> RepoList(
+                        onNavigateToFrom = { currentScreen = "repoFrom" }
+                    )
+                    "repoFrom" -> RepoFrom(
+                        onBackClick = { currentScreen = "repoList"}
                     )
                 }
             }
--- app/src/main/java/ec/edu/puce/githubclient/ui/screens/RepoList.kt
+++ app/src/main/java/ec/edu/puce/githubclient/ui/screens/RepoList.kt
@@ -5,14 +5,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -20,40 +27,58 @@ import ec.edu.puce.githubclient.ui.components.RepoItem
 import ec.edu.puce.githubclient.viewmodels.RepoListViewModel
 
 @Composable
-fun RepoLs(
+fun RepoList(
     modifier: Modifier = Modifier,
-    viewModel: RepoListViewModel = viewModel()
+    viewModel: RepoListViewModel = viewModel(),
+    onNavigateToFrom: () -> Unit = {}
 ) {
 
     val repos by viewModel.repos.collectAsState()
     val isLoading by viewModel.isLoading.collectAsState()
     val errorMsg by viewModel.errorMsg.collectAsState()
 
-    Box(
-        modifier = modifier.fillMaxSize()
-    ) {
-        if (isLoading) {
-            CircularProgressIndicator(
-                modifier = Modifier.align(Alignment.Center)
-            )
+    Scaffold (
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onNavigateToFrom,
+                shape = CircleShape,
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Añadir Repositorio"
+                )
+            }
         }
 
-        errorMsg?.let {
-            Text(
-                text = it,
-                color = MaterialTheme.colorScheme.error,
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .padding(16.dp)
-            )
-        }
+    ) { paddingValues ->
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            if (isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
 
-        if (!isLoading && errorMsg == null) {
+            errorMsg?.let {
+                Text(
+                    text = it,
+                    color = MaterialTheme.colorScheme.error,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(16.dp)
+                )
+            }
 
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-
-                items(repos) { repo ->
-                    RepoItem(repository = repo)
+            if (!isLoading && errorMsg == null) {
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(repos) { repo ->
+                        RepoItem(repository = repo)
+                    }
                 }
             }
         }
@@ -62,6 +87,6 @@ fun RepoLs(
 
 @Preview(showBackground = true)
 @Composable
-fun RepoLsPreview() {
-    RepoLs()
+fun RepoListPreview() {
+    RepoList()
 }
